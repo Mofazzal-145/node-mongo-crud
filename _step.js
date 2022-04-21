d6e3a6212325389c914180cf0e63642139bce17f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-04-22 00:56:59 +0600

    add another project

---
+++ _step.js
@@ -0,0 +1,37 @@
+/* *
+
+
+-------------------
+Create atlas account
+-------------------
+1. sign up with google
+2. create cluster
+3. create user dbUser and password.
+4. Network access > ip address allow
+5. database > connect
+
+------------------
+CRUD Operation
+---------------
+1. node mongodb CRUD > Fundamentals
+2. create async run function
+
+------------
+Integrate sending data from client to server
+-----------
+1. Client side : create form
+2. on submit get from data and create user object. 
+3. on server: create a user POST method to receive data on the backend.
+4. on client side: set fetch with POST, headers, body. 
+5. Make sure you return a json from the POST api.
+ 
+------------
+Load data to the client side
+----------
+1. create get API on the server
+2. create a query object
+3. collection.find(query)
+4. cursor.toArray()
+5. return the result
+
+ */
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+//user:dbuser1
+//password:sJsb0f58tWxLgjo1
+
+
+
+const uri = "mongodb+srv://dbuser1:sJsb0f58tWxLgjo1@cluster0.u9gyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        
+        app.get('/user', async(req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        // POST User: add a new user
+        app.post('/user', async(req, res) =>{
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        });
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+/* client.connect(err => {
+  const collection = client.db("foodExpress").collection("users");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+}); */
+
+
+app.get('/', (req, res) =>{
+    res.send('Running my Node Crud Server');
+});
+
+app.listen(port, () =>{
+    console.log('CRUD Server is Running');
+});
+
+
+
